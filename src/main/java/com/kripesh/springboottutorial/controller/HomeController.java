81df284dbd5ad176cee0cdd07a30b61b7ec9f6fd
kbista <kripeshbista3@gmail.com>
2019-09-19 22:39:45 +0545

    Spring YAML and Properties File Configuration example

--- src/main/java/com/kripesh/springboottutorial/controller/HomeController.java
+++ src/main/java/com/kripesh/springboottutorial/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.kripesh.springboottutorial.controller;
 
+import com.kripesh.springboottutorial.BusinessProperties;
+import com.kripesh.springboottutorial.GlobalProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,14 +17,19 @@ public class HomeController {
 
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
+    @Autowired
+    private BusinessProperties businessProperties;
+
+    @Autowired
+    private GlobalProperties globalProperties;
+
     @GetMapping("/")
     public String hello(Model model) {
 
-        List<String> data = Arrays.asList("Sut", "But", "Kut", "Lut", "Gut");
-        data.forEach(e -> logger.debug("Each Element: {}", e));
-        logger.debug("Hello from Logback {}", data);
-        model.addAttribute("name", data);
-
+        logger.debug("Business Properties {}", businessProperties);
+        logger.debug("Global Properties {}", globalProperties);
+        model.addAttribute("business", businessProperties);
+        model.addAttribute("global", globalProperties);
         return "index";
     }
 }
