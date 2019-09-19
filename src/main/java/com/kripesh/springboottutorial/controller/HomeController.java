35e51fd895e127c79b62160628c96ffe3cf0c889
kbista <kripeshbista3@gmail.com>
2019-09-19 23:05:42 +0545

    Spring Profile YAML VS Properties File example

--- src/main/java/com/kripesh/springboottutorial/controller/HomeController.java
+++ src/main/java/com/kripesh/springboottutorial/controller/HomeController.java
@@ -1,7 +1,6 @@
 package com.kripesh.springboottutorial.controller;
 
-import com.kripesh.springboottutorial.BusinessProperties;
-import com.kripesh.springboottutorial.GlobalProperties;
+import com.kripesh.springboottutorial.config.ServerProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 public class HomeController {
@@ -18,18 +15,13 @@ public class HomeController {
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
-    private BusinessProperties businessProperties;
-
-    @Autowired
-    private GlobalProperties globalProperties;
+    private ServerProperties serverProperties;
 
     @GetMapping("/")
     public String hello(Model model) {
 
-        logger.debug("Business Properties {}", businessProperties);
-        logger.debug("Global Properties {}", globalProperties);
-        model.addAttribute("business", businessProperties);
-        model.addAttribute("global", globalProperties);
+        logger.debug("Server Properties {}", serverProperties);
+        model.addAttribute("server", serverProperties);
         return "index";
     }
 }
